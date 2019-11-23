65d7d71e1074536aa380de1ef7f80e41b47074b7
mihett05 <mihett05@yandex.ru>
2019-11-23 20:13:41 +0500

    Functions from server connection moved to connection

--- server/client.py
+++ server/client.py
@@ -1,8 +1,7 @@
 import socket
 import json
 import hashlib
-import uuid
-
+import threading
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', 0))
@@ -17,15 +16,10 @@ s.send(json.dumps({
 }).encode("utf-8"))
 res = json.loads(s.recv(4096).decode("utf-8"))
 print(res)
-
-if res["status"] == "ok":
-    info = json.dumps({
-        "type": "play",
-        "char_id": res["data"][0][0]
-    }).encode("utf-8")
-    s.sendall(info)
-    print(s.recv(4096).decode("utf-8"))
-while True:
-    if input():
-        break
+info = json.dumps({
+    "type": "play",
+    "char_id": res["data"][0][0]
+}).encode("utf-8")
+s.sendall(info)
+print(json.loads(s.recv(4096).decode("utf-8")))
 s.close()
